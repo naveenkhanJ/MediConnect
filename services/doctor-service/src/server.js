0f611d654c929acee36cc649d350346d959cb397
gayashagimhani <gayashagimhani12@gmail.com>
2026-03-30 21:32:02 +0530

    Add Db connection

--- services/doctor-service/src/server.js
+++ services/doctor-service/src/server.js
@@ -2,12 +2,30 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import app from "./app.js";
+import { connectDB} from "./config/db.js";
+import sequelize from "./config/sequelize.js";
+
+
 dotenv.config();
 
-const app =  express();
-const port = process.env.PORT || 3002;
 
-// SERVER runing
-app.listen(port, () => {
-    console.log(`Doctor service is running on http:localhost:${port}`);
-})
+const PORT = process.env.PORT || 5009;
+
+
+const startServer = async () => {
+    try{
+        await connectDB();
+
+        await sequelize.sync({alter:true});
+
+        app.listen(PORT, () => {
+         console.log(`Doctor service is running on http:localhost:${PORT}`);
+      })
+    }catch (error){
+        console.error("Server startup faile", error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
