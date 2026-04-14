d4b85778566f878911d2e3d05b5aeb1641a787f1
gayashagimhani <gayashagimhani12@gmail.com>
2026-04-15 01:37:22 +0530

    deleted files

--- services/doctor-service/src/routes/doctorTelemedicine.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { startTelemedicineController } from "../controllers/doctorTelemedicine.controller.js";
-
-const router = express.Router();
-
-router.post("/start/:appointmentId",startTelemedicineController);
-
-
-export default router;
