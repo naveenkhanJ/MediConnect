101a4b315d0ceb69fb20e650626bd9d23e217fa6
gayashagimhani <gayashagimhani12@gmail.com>
2026-04-14 23:18:18 +0530

    add mock appointment ,patient servicefor testing

--- services/mock-appointment-service/mock-appointment-service.js
+++ services/mock-appointment-service/mock-appointment-service.js
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 
 
 const app = express();
-const PORT = 5001;
+const PORT = 5003;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -27,7 +27,7 @@ let appointments = [
         patientId: "pat002",
         appointmentDate: "2026-04-06",
         timeSlot: "11:00 AM",
-        status: "CONFIRMED",
+        status: "PENDING",
         paymentStatus: "PAID",
     },
     {
@@ -36,7 +36,7 @@ let appointments = [
         patientId: "pat001",
         appointmentDate: "2026-04-07",
         timeSlot: "09:30 AM",
-        status: "CONFIRMED",
+        status: "PENDING",
         paymentStatus: "FAILED",
     },
 ];
@@ -51,7 +51,7 @@ app.get("/api/appointments/doctor/:doctorId/pending", (req, res) => {
 });
 
 // Approve an appointment
-app.patch("/api/appointments/:appointmentId/status", (req, res) => {
+app.patch("/api/appointments/:appointmentId/decision", (req, res) => {
     const { appointmentId } = req.params;
     const { status } = req.body;
 
@@ -71,6 +71,16 @@ app.get("/api/appointments/:id", (req, res) => {
   res.json(appointment);
 });
 
+// ADD THIS HERE
+app.get("/api/appointments/doctor/:doctorId", (req, res) => {
+    const { doctorId } = req.params;
+
+    const doctorAppointments = appointments.filter(
+        (appt) => appt.doctorId === doctorId
+    );
+
+    res.json(doctorAppointments);
+});
 // Start server
 app.listen(PORT, () => {
     console.log(`Mock Appointment Service running at http://localhost:${PORT}`);
