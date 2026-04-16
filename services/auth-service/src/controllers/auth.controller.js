37f002322930b46cd8a1d86812a4607b1fd436eb
lahirulakmal23 <lahirulakmal893@gmail.com>
2026-04-16 16:22:04 +0530

    create docter endpoint

--- services/auth-service/src/controllers/auth.controller.js
+++ services/auth-service/src/controllers/auth.controller.js
@@ -4,7 +4,25 @@ import { registerService, loginService } from "../services/auth.service.js";
 
 export const register = async (req, res) => {
   try {
-    const { email, password, role, name, age, gender, contact } = req.body;
+    const {
+      email,
+      password,
+      role,
+
+      // common
+      name,
+
+      // patient fields
+      age,
+      gender,
+      contact,
+
+      // doctor fields
+      specialization,
+      experience,
+      license_no,
+      fees
+    } = req.body;
 
     if (!email || !password || !role) {
       return res.status(400).json({
@@ -16,23 +34,48 @@ export const register = async (req, res) => {
     // 1. Create user in Auth DB
     const user = await registerService(email, password, role);
 
-    // 2. If patient → create patient profile
+    // 2. Handle role-based profile creation
     if (role === "patient") {
-      if (!name || age === undefined || age === null || !gender || !contact) {
+      if (!name || age == null || !gender || !contact) {
         return res.status(400).json({
           message: "Validation Error",
-          error: "name, age, gender, and contact are required for patient role"
+          error: "name, age, gender, contact required for patient"
         });
       }
+
       await axios.post("http://localhost:5002/api/patients/register", {
         user_id: user.id,
         email,
-        password,
         name,
         age,
         gender,
         contact
       });
+    }
+
+    else if (role === "doctor") {
+      if (!name || !specialization || !experience || !license_no || !fees ||!contact ) {
+        return res.status(400).json({
+          message: "Validation Error",
+          error: "name, specialization, experience, license_no, fees required for doctor"
+        });
+      }
+
+      await axios.post("http://localhost:5003/api/doctors/register", {
+        user_id: user.id,
+        email,
+        name,
+        specialization,
+        experience,
+        license_no,
+        fees,
+        contact
+      });
+    }
+
+    // admin → no extra service call
+    else if (role === "admin") {
+      // optional: create admin profile service if needed
     }
 
     res.status(201).json({
@@ -42,7 +85,9 @@ export const register = async (req, res) => {
 
   } catch (err) {
     const statusCode = err.statusCode || err.response?.status || 500;
+
     console.log("FULL ERROR:", err.response?.data || err.message);
+
     res.status(statusCode).json({
       message: statusCode >= 500 ? "Server Error" : "Request Failed",
       error: err.response?.data || err.message
