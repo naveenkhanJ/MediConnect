8ae2562ba0134bcb10959ebbb79ad7875b91d75b
gayashagimhani <gayashagimhani12@gmail.com>
2026-04-16 14:34:29 +0530

    add doctor registration

--- services/doctor-service/src/routes/doctorProfile.routes.js
+++ services/doctor-service/src/routes/doctorProfile.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { createProfileController, getProfileController, updateProfileController,getDoctorsBySpecialityController } from "../controllers/doctorProfile.controller.js";
 import { fakeAuth } from "../middlewares/fakeAuth.js";
+import { requireVerifiedDoctor } from "../middlewares/requireVerifiedDoctor.js";
 
 const router = express.Router();
 
-router.post("/",fakeAuth,createProfileController)
-router.get("/me",fakeAuth,getProfileController);
-router.put("/me",fakeAuth,updateProfileController);
-router.get("/search", fakeAuth,getDoctorsBySpecialityController);
+router.post("/",createProfileController)
+router.get("/me",fakeAuth,requireVerifiedDoctor,getProfileController);
+router.put("/me",fakeAuth,requireVerifiedDoctor,updateProfileController);
+router.get("/search", fakeAuth,requireVerifiedDoctor,getDoctorsBySpecialityController);
 
 export default router;
