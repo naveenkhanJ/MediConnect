8fae7a9142f620c8ed175bf972eacd7213ef311b
lahirulakmal23 <lahirulakmal893@gmail.com>
2026-04-17 12:56:15 +0530

    fix api endpoint error

--- services/patient-service/backend/src/routes/patientRoutes.js
+++ services/patient-service/backend/src/routes/patientRoutes.js
@@ -8,7 +8,11 @@ const router = express.Router();
 // Register a new patient
 router.post('/register', registerPatient);
 
-// Internal service-to-service endpoint — returns email + contact only, no user auth required
+// Upload report for a patient
+router.post('/reports', authMiddleware, uploadReport);
+
+//view reports for a patient
+router.get('/reports', authMiddleware, getReports);
 
 // Get patient profile
 router.get('/:id', authMiddleware, getProfile);
@@ -16,11 +20,6 @@ router.get('/:id', authMiddleware, getProfile);
 // Update patient profile
 router.put('/:id', authMiddleware, updateProfile);
 
-// Upload report for a patient
-router.post('/reports', authMiddleware, uploadReport);
-
-//view reports for a patient
-router.get('/reports', authMiddleware, getReports);
 
 //delete patient account
 router.delete('/:id', authMiddleware, deleteAccount);
